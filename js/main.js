824b4ee9d7a71bc9c59aa08acd775433607dcde0
Leon3854 <frad109@mail.ru>
2021-08-12 23:18:31 +0500

    Finish work for the slider goods

--- js/main.js
+++ js/main.js
@@ -1,4 +1,13 @@
 document.body.onclick = function(event) {
   event = event || window.event;
-  console.log(event);
+  // console.log(event);
+  if (event.target.classList.contains('goods-min')) {
+    // remove .active class in .goods-img-min divs
+    var allDeivs = document.querySelectorAll('.goods-img-min div');
+    for (var i = 0; i < allDeivs.length; i++) {
+      allDeivs[i].classList.remove('active');
+    }
+    document.getElementById('goods-max').src = event.target.src;
+    event.target.parentElement.classList.add('active');
+  }
 }
